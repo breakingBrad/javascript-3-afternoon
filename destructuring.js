488d676b4701ac299a9df513577214982c289d7d
breakingBrad <bradvanorman@gmail.com>
2018-05-12 14:04:02 -0600

    destructuring - completed all problems, all tests passing

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,7 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+const { color, make, model, year } = carDetails;
 
 
 
@@ -34,7 +34,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  const { firstName, lastName, title} = obj;
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +53,10 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+function totalPopulation( obj ) {
+  const { utah, california, texas, arizona } = obj;
+  return utah + california + texas + arizona;
+}
 
 
 
@@ -67,8 +70,12 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
-
+function ingredients ( obj ) {
+  const { carb, fat, protein } = obj;
+  const newArr = [];
+  newArr.push(carb, fat, protein);
+  return newArr;
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -85,8 +92,9 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
-
+function largeNumbers( { first, second, third } ) {
+  return Math.min(first, second, third); 
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +106,15 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function numberGroups( { a, b, c } ) {
+  if( a.length > b.length && a.length > c.length ) {
+    return a;
+  } 
+  else if ( b.length > a.length && b.length > c.length ) {
+    return b;
+  }
+  else {
+    return c;
+  }
+}
 
