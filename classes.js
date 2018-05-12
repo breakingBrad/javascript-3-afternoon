cabb2ccc736f553a8d1c9ac64633e6bf44c1f6a0
breakingBrad <bradvanorman@gmail.com>
2018-05-12 13:31:08 -0600

    classes - first 2 problems complete

--- classes.js
+++ classes.js
@@ -29,9 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
-
+class Employee {
+    constructor(first_name, last_name, email, age) {
+      this.first_name = first_name;
+      this.last_name = last_name,
+      this.email = email;
+      this.age = age;
+    }
+    makeWidget() {
+      return this.first_name + ' ' + this.last_name + ' Widget';
+    }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,7 +57,24 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age, reports) {
+    this.first_name = first_name;
+    this.last_name = last_name,
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+  }
+  makeWidget() {
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(i) {
+    this.reports.splice(i, 1);
+  }
+}
 
 
 
